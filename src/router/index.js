79cb43745a6e3a04a02bbc18b3fe0e54163a1025
陈炬 <ju.chen@geely.com>
2024-01-08 20:38:46 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: () => import('../views/home/index.vue'),
+    name: 'Home'
+  },
+  {
+    path: '/three-demo',
+    component: () => import('../views/three-demo/index.vue'),
+    name: 'Home'
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory('/'),
+  routes,
+  scrollBehavior: (to, from, savedPosition) => {
+    console.log('savedPosition', from, savedPosition);
+    if (from.name && savedPosition) {
+      return savedPosition;
+    } else {
+      return { left: 0, top: 0 };
+    }
+  }
+});
+
+export default router;
---
+++ src/views/three-demo/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>demo</div>
+</template>
+<script lang="ts" setup>
+</script>
+<style>
+</style>
